042f6d65ea5cad8c10c70faa2560725cd55b6e29
emmabhl <emma.boehly@epfl.ch>
2024-04-11 11:18:56 +0200

    Changed column names of df

--- DataLoading.R
+++ DataLoading.R
@@ -14,6 +14,6 @@ data[(17:35), 2] <- paste(data[(17:35), 2], data[(17:35), 3])
 ## Get rid of 3rd column
 data <- subset(data, select = -3)
 ## Add corresponding column names 
-colnames(data) <- c("county", "region_name", "region_code", "criminals_per_100k", "ale_beer_houses_per_100k", "attendants_public_school_per_100k", "attendants_public_worship_per_100k")
+colnames(data) <- c("county", "region_name", "region_code", "criminals_per_100k", "ale_beer_houses_per_100k", "attendants_public_school_per_10k", "attendants_public_worship_per_2k")
 ## Saving newly modified object for later use
 save(data, file="projectData.RData")
